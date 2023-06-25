0bbb522b1690f1270c90452e7006eab1862f6d6b
Letian Sun <letian.sun@cerner.com>
2023-06-25 14:14:09 -0700

    Initial commit - move to maven

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import lombok.*;
+public class Main {
+    /**
+     * Finish business logic
+     * Add tests
+     * Lombok
+     *
+     *
+     *
+     * @param args
+     */
+    //Rename to checkout?
+    public static void main(String[] args) {
+
+        //Scanner that reads in data
+        //From data generate a Rental agreement
+        System.out.println("Hello world!");
+        //RentalAgreement agreement = new RentalAgreement();
+    }
+}
